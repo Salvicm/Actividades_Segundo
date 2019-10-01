8305c300f628331624f5830979c1d9e8b63984d6
Salvio Calvillo <salvio.calvillo@enti.cat>
2019-10-01 17:57:17 +0200

    Algo falla arreglar en casa

--- src/AA1_02/Main.cpp
+++ src/AA1_02/Main.cpp
@@ -1,5 +1,6 @@
 #include <SDL.h>		// Always needs to be included for an SDL app
 #include <SDL_image.h> // Imagenes, faltaría SDL_ttf.h para fuentes y SDL_mixer.h para sonido
+#include <SDL_ttf.h>
 
 #include <exception>
 #include <iostream>
@@ -30,6 +31,8 @@ int main(int, char*[])
 	if (!(IMG_Init(imgFlags) & imgFlags)) throw "Error: SDL_image init";
 
 	//-->SDL_TTF
+	if (!TTF_Init()) throw "No es pot inicialitzar SDL_TTF";
+	
 	//-->SDL_Mix
 
 	// --- SPRITES ---
@@ -37,23 +40,36 @@ int main(int, char*[])
 	SDL_Texture* bgTexture{ IMG_LoadTexture(m_renderer, "../../res/img/bg.jpg") };
 	if (bgTexture == nullptr) throw "Error: bgTexture init";
 	SDL_Rect bgRect{ 0,0,SCREEN_WIDTH, SCREEN_HEIGHT };
+
 	//-->Animated Sprite ---
 
 	// --- TEXT ---
+	TTF_Font *font{ TTF_OpenFont("../../res/ttf/saiyan.ttf", 80) };
+	if (font == nullptr)
+		throw "No es pot inicialitzar the TTF_FONT";
+	SDL_Surface *tmpSurf{ TTF_RenderText_Blended(font, "UWU", SDL_Color{ 255,150,0, 255 }) };
+	if (tmpSurf == nullptr) 
+		throw "No es pot crear the SDL text surface";
+	SDL_Texture *textTexture{ SDL_CreateTextureFromSurface(m_renderer, tmpSurf) };
+	SDL_Rect textRect{ 100, 50, tmpSurf->w, tmpSurf->h };
+	SDL_FreeSurface(tmpSurf);
+	TTF_CloseFont(font);
+	//*************
+	//************
 
 	// --- AUDIO ---
 
 	// --- GAME LOOP ---
-	SDL_Event event;
+	SDL_Event event; // Tipo Strüct, maneja ün evento
 	bool isRunning = true;
 	while (isRunning) {
 		// HANDLE EVENTS
-		while (SDL_PollEvent(&event)) {
+		while (SDL_PollEvent(&event)) { // Fünción SDL contiene la cola de todos los eventos, estrüctüra de datos( Pilla siempre el primer elemento y lo elimina de la cola)
 			switch (event.type) {
 			case SDL_QUIT:
 				isRunning = false;
 				break;
-			case SDL_KEYDOWN:
+			case SDL_KEYDOWN: // SOLDADO CAIDO, REPITO: SOLDADO CAÍDO
 				if (event.key.keysym.sym == SDLK_ESCAPE) isRunning = false;
 				break;
 			default:;
@@ -66,16 +82,19 @@ int main(int, char*[])
 		SDL_RenderClear(m_renderer);
 		//Background
 		SDL_RenderCopy(m_renderer, bgTexture, nullptr, &bgRect);
+		SDL_RenderCopy(m_renderer, textTexture, nullptr, &textRect);
 		SDL_RenderPresent(m_renderer);
 
 	}
 
 	// --- DESTROY ---
 	SDL_DestroyTexture(bgTexture);
+	SDL_DestroyTexture(textTexture);
+
 	IMG_Quit();
+	TTF_Quit();
 	SDL_DestroyRenderer(m_renderer);
 	SDL_DestroyWindow(m_window);
-
 	// --- QUIT ---
 	SDL_Quit();
 
