b7c5deaf96949a83533501aa92ddb2be58d96ce6
Salvi Calvillo <Salvio.calvillo@enti.cat>
2019-10-08 00:35:37 +0200

    Whatever

--- src/AA1_02_SDL/Main.cpp
+++ src/AA1_02_SDL/Main.cpp
@@ -6,35 +6,14 @@
 int main(int, char*[]) {
 	
 	// inits
-#pragma region INITS
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		throw "Unable to initialize SDL";
-	}
-	if (TTF_Init() != 0) {
-		throw
-			"Unable to initialize TTF";
-	}
-	const Uint8 imgFlags{ IMG_INIT_JPG | IMG_INIT_PNG };
-	if (!IMG_Init(imgFlags) & imgFlags) {
-		throw "Unable to initialize IMG";
-	}
-	const Uint8 mixFlags{ MIX_INIT_MP3 };
-	if (!(Mix_Init(mixFlags) & mixFlags)) {
-		throw "Error: SDL_mixer init";
-	}
+	graphicHelper::initLibrary();
 	// Window/Renderer
-	SDL_Window *m_window = { SDL_CreateWindow("AA1_02_pt3...", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN) };
-	if (m_window == nullptr) {
-		throw "Unable to create window";
-	}
-	SDL_Renderer *m_renderer = { SDL_CreateRenderer(m_window,-1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC) };
-	if (m_renderer == nullptr) {
-		throw "Unable to create renderer";
-	}
-	
-#pragma endregion
+	SDL_Renderer *m_renderer;
+	SDL_Window *m_window;
+	graphicHelper::startWindowAndRender(&m_window, &m_renderer);
 
 	// --- SPRITES ---
+
 	//Background
 	SDL_Texture* bgTexture{ IMG_LoadTexture(m_renderer, "../../res/img/bg.jpg") };
 	if (bgTexture == nullptr) throw "Error: bgTexture init";
--- src/AA1_02_SDL/graphLibUtils.h
+++ src/AA1_02_SDL/graphLibUtils.h
@@ -8,10 +8,31 @@
 namespace graphicHelper {
 	// Tienen que ser estáticas al no formar parte de un objeto?
 	static void initLibrary() {
-
+		if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+			throw "Unable to initialize SDL";
+		}
+		if (TTF_Init() != 0) {
+			throw
+				"Unable to initialize TTF";
+		}
+		const Uint8 imgFlags{ IMG_INIT_JPG | IMG_INIT_PNG };
+		if (!IMG_Init(imgFlags) & imgFlags) {
+			throw "Unable to initialize IMG";
+		}
+		const Uint8 mixFlags{ MIX_INIT_MP3 };
+		if (!(Mix_Init(mixFlags) & mixFlags)) {
+			throw "Error: SDL_mixer init";
+		}
 	}
 
-	static void startWindowAndRender() {
-
+	static void startWindowAndRender(SDL_Window **m_window, SDL_Renderer **m_renderer) {
+		*m_window = { SDL_CreateWindow("AA1_02_pt3...", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN) };
+		if (m_window == nullptr) {
+			throw "Unable to create window";
+		}
+		*m_renderer = { SDL_CreateRenderer(*m_window,-1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC) };
+		if (m_renderer == nullptr) {
+			throw "Unable to create renderer";
+		}
 	}
 }
